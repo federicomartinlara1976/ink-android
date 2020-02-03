3ac735cef367ec68c55942ef7c46b2c67dcfa874
Federico Martín Lara <federicomartinlara1976@gmail.com>
2020-02-03 15:44:45 +0100

    Update SignActionListener.java

--- sample/src/main/java/com/simplify/ink/sample/SignActionListener.java
+++ sample/src/main/java/com/simplify/ink/sample/SignActionListener.java
@@ -2,5 +2,19 @@ package com.simplify.ink.sample;
 
 import android.view.View;
 
-public class SignActionListener {
+public class SignActionListener implements View.OnClickListener {
+  
+  private MainActivity mainActivity;
+  
+  private FileOperations operations;
+  
+  public ClearActionListener(MainActivity mainActivity, FileOperations operations) {
+    this.mainActivity = mainActivity;
+    this.operations = operations;
+  }
+ 
+  @Override
+  public void onClick(View v) {
+    
+  }
 }
