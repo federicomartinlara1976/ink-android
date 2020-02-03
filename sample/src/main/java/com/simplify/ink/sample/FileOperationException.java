57a30bcc7792c4c6ad081667684ca4aa130cb54c
Federico Martín Lara <federicomartinlara1976@gmail.com>
2020-02-03 11:35:54 +0100

    Create FileOperationException.java

---
+++ sample/src/main/java/com/simplify/ink/sample/FileOperationException.java
@@ -0,0 +1,22 @@
+
+
+public class FileOperationException extends Exception {
+    public FileOperationException() {
+    }
+
+    public FileOperationException(String message) {
+        super(message);
+    }
+
+    public FileOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileOperationException(Throwable cause) {
+        super(cause);
+    }
+
+    public FileOperationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
